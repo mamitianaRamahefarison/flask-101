9bad33361eb9d1f81e1779d3a17baa8dcea7423d
Haja RANDRIANARISOA <hajaran@connecteo.mg>
2022-03-16 14:19:55 +0300

    Adding /api/v1/products endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 # wsgi.py
 # pylint: disable=missing-docstring
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello every body!"
+@app.route('/api/v1/products')
+def PRODUCTS():
+    PRODUCTS = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+}
+    return PRODUCTS
